d1e8f05a371b772c2d5191661c4485d118f09066
Eddie Ruano <eddieruano@gmail.com>
2017-06-01 08:44:12 -0700

    added DESI

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-01 08:32:57
+# @Last Modified time: 2017-06-01 08:43:51
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -12,6 +12,7 @@ import os
 import time
 import RPi.GPIO as GPIO
 import VoyagerHCSR04
+import MPR121
 import DESIConfig
 
 ### Set path ###
@@ -21,7 +22,7 @@ TOP_DIR = os.path.dirname(os.path.abspath(__file__))
 DESI = DESIConfig.DESI()
 Voyager1 = VoyagerHCSR04.Voyager("Voyager1", DESI.PROX1_TRIG, DESI.PROX1_ECHO)
 Voyager2 = VoyagerHCSR04.Voyager("Voyager2", DESI.PROX2_TRIG, DESI.PROX2_ECHO)
-
+TouchSense = MPR121.MPR121()
 
 def main():
     
@@ -29,12 +30,20 @@ def main():
     DESI.initDESI()
     # Initialize Voyager Proximity Sensors
     DESI.initProximity(Voyager1, Voyager2)
+    # Initialize TouchSense Capacitive Sensor Array
+    # Initialize communication with MPR121 using default I2C bus of device, and
+    # default I2C address (0x5A).  
+    if not TouchSense.begin():
+        print('Error initializing MPR121.  Check your wiring!')
+        sys.exit(1)
+
+    """Starts Main Workout Loop"""
+    ActiveFlag = True
     try:
-        while True:
+        while ActiveFlag:
             distv1 = Voyager1.get_distance()
             print ("Measured Distance = %.1f cm" % distv1)
             time.sleep(1)
- 
     # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Shutdown Mission.")
