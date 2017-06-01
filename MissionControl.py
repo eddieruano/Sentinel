75076b3b6c2814ba6e8e10af91b678a5891cc919
Eddie Ruano <eddieruano@gmail.com>
2017-06-01 10:42:32 -0700

    newswig

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-01 10:25:15
+# @Last Modified time: 2017-06-01 10:34:58
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -17,7 +17,7 @@ import RPi.GPIO as GPIO
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import tresources.VoyagerHCSR04
 import tresources.DESIConfig
-import snowboydecoder
+#import snowboydecoder
 
 ### Set path ###
 ### Global Variables ###
@@ -26,9 +26,9 @@ Voyager1 = VoyagerHCSR04.Voyager("Voyager1", DESI.PROX1_TRIG, DESI.PROX1_ECHO)
 Voyager2 = VoyagerHCSR04.Voyager("Voyager2", DESI.PROX2_TRIG, DESI.PROX2_ECHO)
 TouchSense = MPR121.MPR121()
 ### Begin Voice Detection Config ###
-HotwordInterrupt = False
-TriggerWord = DESI.pmdl
-Detector = snowboydecoder.HotwordDetector(TriggerWord, sensitivity=0.5)
+#HotwordInterrupt = False
+#TriggerWord = DESI.pmdl
+#Detector = snowboydecoder.HotwordDetector(TriggerWord, sensitivity=0.5)
 
 def main():
     # Initialize DESI States
@@ -39,13 +39,13 @@ def main():
     # Initialize comms with MPR121 using default I2C bus of device, and
     # default I2C address (0x5A).  
     if not TouchSense.begin():
-        print('Error initializing MPR121')
+        print('Error initializing MPR121.')
         sys.exit(1)
     # Voice Detection
-    signal.signal(signal.SIGINT, signal_handler)
-    detector.start(detected_callback=snowboydecoder.play_audio_file,
-               interrupt_check=interrupt_callback,
-               sleep_time=0.03)
+    #signal.signal(signal.SIGINT, signal_handler)
+    #detector.start(detected_callback=snowboydecoder.play_audio_file,
+    #           interrupt_check=interrupt_callback,
+    #           sleep_time=0.03)
     """Starts Main Workout Loop"""
     ActiveFlag = True
     try:
@@ -64,12 +64,12 @@ def activateAlexa():
     GPIO.output(DESI.OUT_ALEXA, GPIO.LOW)
     time.sleep(2)
     GPIO.output(DESI.OUT_ALEXA, GPIO.HIGH)
-def signal_handler(signal, frame):
-    global HotwordInterrupt
-    HotwordInterrupt = True
-def interrupt_callback():
-    global HotwordInterrupt
-    return HotwordInterrupt
+#def signal_handler(signal, frame):
+#    global HotwordInterrupt
+#    HotwordInterrupt = True
+#def interrupt_callback():
+#    global HotwordInterrupt
+#    return HotwordInterrupt
 ### MAIN CALL ###
 if __name__ == "__main__":
     main()
