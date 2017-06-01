a50add25379e7b8d41d78a250542a4e98797b7e2
Eddie Ruano <eddieruano@gmail.com>
2017-06-01 10:11:46 -0700

    snowboy fixes

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-01 10:08:30
+# @Last Modified time: 2017-06-01 10:11:25
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -17,7 +17,9 @@ import RPi.GPIO as GPIO
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import resources.VoyagerHCSR04
 import resources.DESIConfig
+import resouces.snowboy.snowboydetect
 import resources.snowboy.snowboydecoder
+
 
 
 
