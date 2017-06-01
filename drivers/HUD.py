3d42dedd714db5304bb125f344857f695c43b7e7
Eddie Ruano <eddieruano@gmail.com>
2017-06-01 13:05:00 -0700

    added curses

--- drivers/HUD.py
+++ drivers/HUD.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-06-01 12:03:30
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-01 12:53:57
+# @Last Modified time: 2017-06-01 13:04:53
 
 import curses
 
@@ -22,15 +22,12 @@ class HUD(object):
         self.display.border(0)
         self.displayHeaderBar()
         # Create Left Window
-        #self.leftBox = self.display.subwin(self.v_box_ht, self.v_box_wt, 5, 5)
         self.leftBox.box()
         # Create Right Window
-        #self.rightBox = self.display.subwin(self.v_box_ht, self.v_box_wt, 5, 40)
         self.rightBox.box()
         self.leftBox.addstr(1, 5, "Voyager 1 Distance")
         self.rightBox.addstr(1, 5, "Voyager 2 Distance")
         # Create MidSetion Status
-        #self.midBox = self.display.subwin(10, 60, 10, 10)
         self.midBox.box()
         self.midBox.addstr(1, 21, "Control Status")
         self.midBox.addstr(3, 5, "Voyager Disparity Error +/-: ")
@@ -38,9 +35,7 @@ class HUD(object):
         self.midBox.addstr(5, 5, "ProxV2 Status: ")
         self.midBox.addstr(6, 5, "SlowDown Factor: 0x (in Green)")
         self.midBox.addstr(7, 5, "Timeout: 0 (in Green)")
-        
         # Create Touch Box
-        #self.touchBox = self.display.subwin(10, 60, 25, 10)
         self.touchBox.box()
         self.touchBox.addstr(1, 21, "Cap Touch Status")
         self.displayRefresh()
@@ -58,3 +53,19 @@ class HUD(object):
         self.rightBox.refresh()
         self.midBox.refresh()
         self.touchBox.refresh()
+    def displayBar (self, iteration):
+        total = 30
+        prefix = 'StartZone'
+        suffix = 'RedZone'
+        fill = '█'
+        decimals = 1
+        length = 35
+        # Do work
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        end = "%\r"
+        buf = "%s |%s| %s %s %%\n" % (prefix, bar, suffix, percent)
+        self.display.addstr(22, 5, buf)
+        self.displayRefresh()
+    def displayInfo(self, prox1, prox2, status):
