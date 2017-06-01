2c461436f66a32750dae042217f68eb3881a6c39
Eddie Ruano <eddieruano@gmail.com>
2017-06-01 12:31:35 -0700

    added curses

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-01 12:29:30
+# @Last Modified time: 2017-06-01 12:31:29
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
