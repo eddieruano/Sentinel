520276496bcf73c150a4c965d4a550a125bca312
Eddie Ruano <eddieruano@gmail.com>
2017-06-01 10:15:09 -0700

    snowboy fixes

--- resources/snowboy/snowboydecoder.py
+++ resources/snowboy/snowboydecoder.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
+import sys
+import os.path
 import collections
 import pyaudio
-#import snowboydetect
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import snowboydetect
 import time
 import wave
 import os
