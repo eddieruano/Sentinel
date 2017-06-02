439c50b8ea08929aa02284609996e935e95bc6e2
Eddie Ruano <eddieruano@gmail.com>
2017-06-02 03:52:04 -0700

    Script

---
+++ Dash.py
@@ -0,0 +1,28 @@
+ import cayenne.client
+import time
+
+# Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+MQTT_USERNAME  = "b1a32a40-4747-11e7-b5e5-0383e230cbfa"
+MQTT_PASSWORD  = "69ce3f90356b85ae6458cfb19188b405064b2f3b"
+MQTT_CLIENT_ID = "1d6115d0-477f-11e7-b5e5-0383e230cbfa"
+
+# The callback for when a message is received from Cayenne.
+def on_message(message):
+  print("message received: " + str(message))
+  # If there is an error processing the message return an error string, otherwise return nothing.
+
+client = cayenne.client.CayenneMQTTClient()
+client.on_message = on_message
+client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID)
+
+i=0
+timestamp = 0
+
+while True:
+  client.loop()
+  if (time.time() > timestamp + 10):
+    client.celsiusWrite(1, i)
+    client.luxWrite(2, i*10)
+    client.hectoPascalWrite(3, i+800)
+    timestamp = time.time()
+    i = i+1
