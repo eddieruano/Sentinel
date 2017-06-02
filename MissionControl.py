d7c39c5e4acf09558679d03493769b1f4823e2ae
Eddie Ruano <eddieruano@gmail.com>
2017-06-02 12:59:50 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-02 12:39:30
+# @Last Modified time: 2017-06-02 12:59:19
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -41,7 +41,15 @@ def main():
     #    print("TSense")
     #    sys.exit(1)
     try:
-        DESI.DESIListen()
+        print("Listening")
+        GPIO.add_event_detect(DESI.IN_START, GPIO.FALLING, DESI.performStart, DESI.bounceTime)
+        GPIO.add_event_detect(DESI.IN_PAUSE, GPIO.FALLING, DESI.performPause, DESI.bounceTime)
+        GPIO.add_event_detect(DESI.IN_SPEED0, GPIO.FALLING, DESI.perform00, DESI.bounceTime)
+        GPIO.add_event_detect(DESI.IN_SPEED1, GPIO.FALLING, DESI.perform01, DESI.bounceTime)
+        GPIO.add_event_detect(DESI.IN_SPEED2, GPIO.FALLING, DESI.perform02, DESI.bounceTime)
+        GPIO.add_event_detect(DESI.IN_SPEED3, GPIO.FALLING, DESI.perform03, DESI.bounceTime)
+        GPIO.add_event_detect(DESI.IN_SPEED4, GPIO.FALLING, DESI.perform04, DESI.bounceTime)
+        #DESI.DESIListen()
         activeFlag = True
         while activeFlag == True:
             activeFlag = True
