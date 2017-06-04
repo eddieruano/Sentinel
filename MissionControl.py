1cfc24818255b2f0823757d56f0bbdf25482235f
Eddie Ruano <eddieruano@gmail.com>
2017-06-04 00:40:56 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-04 00:25:35
+# @Last Modified time: 2017-06-04 00:40:54
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -15,6 +15,7 @@ import collections
 import pyaudio
 import wave
 import time
+import curses
 from math import floor
 # Customs Mods #
 import Adafruit_MPR121.MPR121 as MPR121
@@ -31,7 +32,7 @@ DESI = DESIConfig.DESI()
 Voyager1 = VoyagerHCSR04.Voyager("Voyager1", DESI.PROX1_TRIG, DESI.PROX1_ECHO)
 Voyager2 = VoyagerHCSR04.Voyager("Voyager2", DESI.PROX2_TRIG, DESI.PROX2_ECHO)
 TouchSense = MPR121.MPR121()
-CONST_REDUX = 0.2
+CONST_REDUX = 0.1
 CONST_ZONE_FIX = 0.0
 
 def main():
@@ -49,6 +50,11 @@ def main():
     if not TouchSense.begin():  # Init TouchSense Capacitive Sensor Array
         print("TSense")
         sys.exit(1)
+    stdscr = curses.initscr()
+    stdscr.border(0)
+    curses.noecho()
+    stdscr.refresh()
+    curses.wrapper(main)
     try:
         print("Listening")
         GPIO.add_event_detect(DESI.IN_START, GPIO.FALLING, DESI.performStart, DESI.Time_Bounce)
@@ -61,71 +67,77 @@ def main():
         #DESI.DESIListen()
         activeFlag = True
         while activeFlag == True:
-            activeFlag = True
-            command = input("Enter a command: ")
-            if command == "start":
-                play_audio_file(DING)
-                DESI.DESISend("Start")
-            elif command == "shutdown":
-                DESI.DESISend("Shutdown")
-            elif command == "pause":
-                DESI.DESISend("Pause")
-            elif command == "unpause":
-                DESI.DESISend("Pause")
-            elif command == "enter":
-                DESI.DESISend("Enter")
-            elif command == "0":
-                DESI.DESISend("Send00")
-            elif command == "1":
-                DESI.DESISend("Send01")
-            elif command == "2":
-                DESI.DESISend("Send02")
-            elif command == "3":
-                DESI.DESISend("Send03")
-            elif command == "4":
-                DESI.DESISend("Send04")
-            elif command == "down":
-                DESI.DESISend("SendDown")
-            elif command == "alexa":
-                DESI.DESISend("SendAlexa")
-            else:
-                print("Invalid Command")
-            print(DESI.State_Main)
-            # Query for the proximity of Megan #
-            #time.sleep(0.3)
-            ave = queryDistance()
-            print(ave)
-            
-            if(ave > DESI.Zone_Yellow):
-                subRedux = DESI.State_Speed * CONST_REDUX * 10
-                subZone = floor(ave - DESI.Zone_Yellow) + 1.0
-                redux = subZone * subRedux
-                print(subRedux)
-                print(subZone)
-                print(redux)
-            #distAverage = (distv1 + distv2) / 2
-            #proxError = distv1 - distv2
-            #contact = checkContact()
-            #state = checkState(distAverage)
-            #if state == "Red":
-            #    print("Red!")
-            #    past_state = state
-            #    DESI.send("Pause")
-            #    time.sleep(5)
-            #    DESI.send(past_state)
-            #elif state == "Yellow":
-            #    DESI.send("Down")
-            #    time.sleep(0.4)
-            #    DESI.send("Down")
-            #    time.sleep(0.4)
-            #    DESI.send("Down")
-            #else:
-            #    print("Green!")
-            #    slack = 30 - distAverage
-            #    redux = ((distAverage - 10) / 2) * 10
-            #    redux = int(redux)
-#           time.sleep(1)
-    # Catch Ctrl+C
+            try:
+                try:
+                    command = stdscr.getkey()
+                    #command = input("Enter a command: ")
+                    if command == "s":
+                        play_audio_file(DING)
+                        DESI.DESISend("Start")
+                    elif command == "h":
+                        DESI.DESISend("Shutdown")
+                    elif command == "p":
+                        DESI.DESISend("Pause")
+                    elif command == "u":
+                        DESI.DESISend("Pause")
+                    elif command == "e":
+                        DESI.DESISend("Enter")
+                    elif command == "0":
+                        DESI.DESISend("Send00")
+                    elif command == "1":
+                        DESI.DESISend("Send01")
+                    elif command == "2":
+                        DESI.DESISend("Send02")
+                    elif command == "3":
+                        DESI.DESISend("Send03")
+                    elif command == "4":
+                        DESI.DESISend("Send04")
+                    elif command == "d":
+                        DESI.DESISend("SendDown")
+                    elif command == "a":
+                        DESI.DESISend("SendAlexa")
+                    else:
+                        print("refresh")
+                    print(DESI.State_Main)
+                except:
+                    time.sleep(0.2)
+                    # Query for the proximity of Megan #
+                    #time.sleep(0.3)
+                    ave = queryDistance()
+                    print(ave)
+                    
+                    if(ave > DESI.Zone_Yellow):
+                        subRedux = DESI.State_Speed * CONST_REDUX * 10
+                        subZone = floor(ave - DESI.Zone_Yellow) + 1.0
+                        redux = subZone * subRedux
+                        print(subRedux)
+                        print(subZone)
+                        print(redux)
+                        for i in redux:
+                            DESISend("SendDown")
+                    #distAverage = (distv1 + distv2) / 2
+                    #proxError = distv1 - distv2
+                    #contact = checkContact()
+                    #state = checkState(distAverage)
+                    #if state == "Red":
+                    #    print("Red!")
+                    #    past_state = state
+                    #    DESI.send("Pause")
+                    #    time.sleep(5)
+                    #    DESI.send(past_state)
+                    #elif state == "Yellow":
+                    #    DESI.send("Down")
+                    #    time.sleep(0.4)
+                    #    DESI.send("Down")
+                    #    time.sleep(0.4)
+                    #    DESI.send("Down")
+                    #else:
+                    #    print("Green!")
+                    #    slack = 30 - distAverage
+                    #    redux = ((distAverage - 10) / 2) * 10
+                    #    redux = int(redux)
+            #           time.sleep(1)
+            # Catch Ctrl+C
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("Shutdown Mission.")
