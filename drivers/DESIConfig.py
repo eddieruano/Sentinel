7c03251a3e5beefa5e0a24fdd1f9af87ac54df84
Eddie Ruano <eddieruano@gmail.com>
2017-06-03 20:22:44 -0700

    Script

--- drivers/DESIConfig.py
+++ drivers/DESIConfig.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-06-01 07:23:39
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-03 20:17:37
+# @Last Modified time: 2017-06-03 20:22:36
 
 import RPi.GPIO as GPIO
 import time
