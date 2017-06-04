40c7f2e971b3205c3333eba5f89a83f0c8d28eed
Eddie Ruano <eddieruano@gmail.com>
2017-06-04 01:15:32 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-04 01:14:07
+# @Last Modified time: 2017-06-04 01:15:30
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -55,7 +55,7 @@ def main():
         sys.exit(1)
     try:
         print("Listening")
-        GPIO.add_event_detect(DESI.IN_START, GPIO.FALLING, DESI.performStart, DESI.Time_Bounce)
+        GPIO.add_event_detect(DESI.IN_START, GPIO.FALLING, performStart, DESI.Time_Bounce)
         GPIO.add_event_detect(DESI.IN_PAUSE, GPIO.FALLING, DESI.performPause, DESI.Time_Bounce)
         GPIO.add_event_detect(DESI.IN_SPEED0, GPIO.FALLING, DESI.performS0, DESI.Time_Bounce)
         GPIO.add_event_detect(DESI.IN_SPEED1, GPIO.FALLING, DESI.performS1, DESI.Time_Bounce)
