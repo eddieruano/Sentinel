98d2502574e49e99862269e5d79a46d6fadaa1a4
Eddie Ruano <eddieruano@gmail.com>
2017-06-04 00:57:37 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-04 00:56:54
+# @Last Modified time: 2017-06-04 00:57:35
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -101,7 +101,7 @@ def main():
                     print(subRedux)
                     print(subZone)
                     print(redux)
-                    while (i < redux) and self.dFlag:
+                    while (i < redux) and dFlag:
                         DESI.DESISend("Down")
                         dFlag = False    
                     print("refresh")
