4be27c9557a6145aea0a6aedec8f96d8ff0b4b8f
Eddie Ruano <eddieruano@gmail.com>
2017-06-04 00:45:48 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-04 00:43:52
+# @Last Modified time: 2017-06-04 00:45:37
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -53,6 +53,7 @@ def main(stdscr):
     stdscr = curses.initscr()
     stdscr.border(0)
     curses.noecho()
+    curses.use_default_colors()
     stdscr.refresh()
     try:
         print("Listening")
