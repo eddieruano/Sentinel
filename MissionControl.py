2b236e4f0bc2e6c7b0a731cddd7fc27e6bdb78c1
Eddie Ruano <eddieruano@gmail.com>
2017-06-04 02:16:35 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-04 02:13:15
+# @Last Modified time: 2017-06-04 02:16:31
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -117,7 +117,7 @@ def main():
             ave = queryDistance()
             print(ave)
             if ave == -1.0:
-                print "Error"
+                print ("Error")
             elif(ave > DESI.Zone_Yellow):
                 subRedux = DESI.State_Speed * CONST_REDUX * 10
                 subZone = floor(ave - DESI.Zone_Yellow) + 1.0
