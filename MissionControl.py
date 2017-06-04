0b8c39dbf1380938cedae968cd0e73633bf08124
Eddie Ruano <eddieruano@gmail.com>
2017-06-04 03:28:03 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-04 03:27:48
+# @Last Modified time: 2017-06-04 03:28:02
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -99,10 +99,10 @@ def main():
                 DESI.DESISend("Send04")
             else:
                 pass
-                
+
             Proximity = queryDistance()
             if FlagDisparity:
-                self.FlagDisparity = False;
+                FlagDisparity = False;
             print(Proximity)
             subZone = floor(Proximity - DESI.Zone_Yellow) + 1.0
             if Proximity == -1.0:
