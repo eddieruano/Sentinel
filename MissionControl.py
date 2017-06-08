039da19bdef36d77f3ba1041d02ca4ed48ada81d
Eddie Ruano <eddieruano@gmail.com>
2017-06-08 09:21:06 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-08 09:19:34
+# @Last Modified time: 2017-06-08 09:20:14
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -31,7 +31,7 @@ import drivers.MPR121 as MPR121
 DESI = DESIConfig.DESI()
 Voyager1 = VoyagerHCSR04.Voyager("Voyager1", DESI.PROX1_TRIG, DESI.PROX1_ECHO)
 Voyager2 = VoyagerHCSR04.Voyager("Voyager2", DESI.PROX2_TRIG, DESI.PROX2_ECHO)
-#TouchSense = MPR121.MPR121()
+TouchSense = MPR121.MPR121()
 Sentinel = Sentinel.Sentinel()
 ################################## PATHS ######################################
 
