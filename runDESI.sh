c70ee2cf2c381662aa1e9c98a07b7ce9b4c14d0d
Eddie Ruano <eddieruano@gmail.com>
2017-06-09 03:25:38 -0700

    Script

--- runDESI.sh
+++ runDESI.sh
@@ -3,4 +3,4 @@ echo "Starting"
 cd ~/Desktop/Sentinel
 git pull
 echo "Pulling"
-sudo python3 ~/Desktop/Sentinel/MissionControl.py && sudo python3 ~/Desktop/Sentinel/triggers/alexaSnow.py resources/DESI.pmdl
+sudo python3 ~/Desktop/Sentinel/MissionControl.py
