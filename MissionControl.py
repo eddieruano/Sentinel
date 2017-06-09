4558919c97b79c04907dc092f7ddffe0fb7ec90d
Eddie Ruano <eddieruano@gmail.com>
2017-06-08 22:04:17 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-08 22:03:18
+# @Last Modified time: 2017-06-08 22:04:11
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -54,7 +54,7 @@ def main():
         sys.exit(1)
     Sentinel.getStateKnob(DESI)
     Sentinel.setStateKnob()
-    time.delay(1)
+    time.sleep(1)
     Sentinel.getStateKnob(DESI)
     Sentinel.setStateKnob()
     Sentinel.updateActiveLock(TouchSense)
