ede822f8b327b6ffa588edff1fab058615bcd1b4
Eddie Ruano <eddieruano@gmail.com>
2017-06-08 20:52:22 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-08 20:51:03
+# @Last Modified time: 2017-06-08 20:52:20
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
@@ -110,6 +110,7 @@ def main():
                 if Sentinel.CapCountdown == 0:
                     DESI.DESISend("Pause")
                     print("la")
+                    DESI.State_Main = "Pause"
                     print(DESI.State_Main)
                     Sentinel.ActiveLock = True
                 else:
