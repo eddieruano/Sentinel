fa044426faa836a24c83e3ac741b67893385015a
Eddie Ruano <eddieruano@gmail.com>
2017-06-08 21:08:31 -0700

    Script

--- MissionControl.py
+++ MissionControl.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-06-08 21:04:57
+# @Last Modified time: 2017-06-08 21:08:17
 # 
 """
     MissionControl.py is a debugging tool for DESI_Sentinel
